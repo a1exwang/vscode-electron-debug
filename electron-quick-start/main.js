66dbf29723602a64546786db323d3955b4965495
Aocheng Wang <aochengwang@microsoft.com>
2022-10-28 13:15:47 +0800

    chore: test debug webview/iframe/browserview in vscode

--- electron-quick-start/main.js
+++ electron-quick-start/main.js
@@ -1,16 +1,32 @@
 const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const { app, BrowserWindow, BrowserView } = electron;
+app.commandLine.appendSwitch ("disable-http-cache");
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 1000,
+    webPreferences: {
+      webviewTag: true,
+      allowVideoRendererInSandbox: true,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: false,
+    },
+  });
+  mainWindow.webContents.on('will-attach-webview', (e, webPreferences, ps) => {
+    // webPreferences.preload = utils.getPreloadPath('src/renderer/video-extensibility/videoapp/preload.js', true);
+    webPreferences.contextIsolation = true;
+    webPreferences.allowVideoRendererInSandbox = true;
+    webPreferences.allowVideoExtensibilityInSandbox = true;
+    webPreferences.sandbox = true;
+    webPreferences.worldSafeExecuteJavaScript = true;
+    webPreferences.additionalArguments = ['--enable-precise-memory-info'];
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
@@ -25,29 +41,36 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  // const window1 = new BrowserWindow({
+  //   title: "Browser Window 1",
+  //   width: 400,
+  //   height: 300,
+  // });
+  // window1.loadURL("http://localhost:8080/index.html");
+
+  // For some reason, creating BrowserView has some timing issue. Use a timeout to workaround.
+  setTimeout(createBrowserView, 500);
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+function createBrowserView() {
+  const view = new BrowserView();
+  mainWindow.setBrowserView(view);
+  view.setBounds({ x: 0, y: 550, width: 300, height: 300 });
+  view.setBackgroundColor("#ecc");
+  view.webContents.loadURL("http://localhost:8080/index.html");
+}
+
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
